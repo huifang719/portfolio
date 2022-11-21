eba995d1ba85122939717a480a9e572c53fcfcc6
huifang <hui-f.gu@hotmail.com>
2022-11-22 08:59:54 +1030

    update skill layout

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import { Col, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import './skills.css'
 import css from "./images/css.png"
 import javascript from "./images/js.png"
@@ -49,119 +49,121 @@ function Skills() {
   }
 
   return(
-    <Row xs={1} md={3}>
-      <Col style={{border:"1px solid lightgrey", height:"70vh",color:"lightgrey"}}>
-        <h3 className="text-center">Languages</h3>
-        <motion.img src={javascript} id="js" style={{width:"20%"}} 
-          variants={skillsVariants}
-          initial='hidden'
-          animate='visible'
-          whileHover="hover">
-        </motion.img>  
-        <motion.img src={python} id="python" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover">  
-        </motion.img>
-        <motion.img src={ruby} id="ruby" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover='hover'>
-        </motion.img>
-        <motion.img src={html} id="html" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover='hover'>
-        </motion.img>
-        <motion.img src={css} id="css" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover='hover'>
-        </motion.img>
-        <motion.img src={psql} id="psql" style={{width:"20%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover='hover'>
-        </motion.img>
-      </Col>
-      <Col style={{border:"1px solid lightgrey", height:"70vh", color:"lightgrey"}}>
-        <h3 className="text-center">Frameworks & Libraries</h3>
-        <motion.img src={react} id="react" style={{width:"20%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>
-        <motion.img src={bootstrap} id="bootstrap" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>     
-        <motion.img src={nodejs} id="nodejs" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>  
-        <motion.img src={express} id="express" style={{width:"20%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>  
-        <motion.img src={sinatra} id="sinatra" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>  
-        <motion.img src={jest} id="jest" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img> 
-        <motion.img src={fm} id="fm" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img> 
-      </Col>
-      <Col style={{border:"1px solid lightgrey", height:"70vh",color:"lightgrey"}}>
-        <h3 className="text-center">Other Skills</h3>
-        <motion.img src={ps} id="ps" style={{width:"20%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>
-        <motion.img src={indesign} id="indesign" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover">          
-        </motion.img>
-        <motion.img src={illustrator} id="illustrator" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>
-        <motion.img src={pr} id="pr" style={{width:"15%"}}
-          variants={skillsVariants}
-          initial="hidden"
-          animate="visible"
-          whileHover="hover"> 
-        </motion.img>
-      </Col>
-    </Row>
+    <Container>
+      <Row xs={1} md={3}>
+        <Col style={{border:"1px solid lightgrey", height:"70vh",color:"lightgrey"}}>
+          <h3 className="text-center">Languages</h3>
+          <motion.img src={javascript} id="js" style={{width:"20%"}} 
+            variants={skillsVariants}
+            initial='hidden'
+            animate='visible'
+            whileHover="hover">
+          </motion.img>  
+          <motion.img src={python} id="python" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover">  
+          </motion.img>
+          <motion.img src={ruby} id="ruby" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover='hover'>
+          </motion.img>
+          <motion.img src={html} id="html" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover='hover'>
+          </motion.img>
+          <motion.img src={css} id="css" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover='hover'>
+          </motion.img>
+          <motion.img src={psql} id="psql" style={{width:"20%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover='hover'>
+          </motion.img>
+        </Col>
+        <Col style={{border:"1px solid lightgrey", height:"70vh", color:"lightgrey"}}>
+          <h3 className="text-center">Frameworks & Libraries</h3>
+          <motion.img src={react} id="react" style={{width:"20%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>
+          <motion.img src={bootstrap} id="bootstrap" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>     
+          <motion.img src={nodejs} id="nodejs" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>  
+          <motion.img src={express} id="express" style={{width:"20%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>  
+          <motion.img src={sinatra} id="sinatra" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>  
+          <motion.img src={jest} id="jest" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img> 
+          <motion.img src={fm} id="fm" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img> 
+        </Col>
+        <Col style={{border:"1px solid lightgrey", height:"70vh",color:"lightgrey"}}>
+          <h3 className="text-center">Other Skills</h3>
+          <motion.img src={ps} id="ps" style={{width:"20%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>
+          <motion.img src={indesign} id="indesign" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover">          
+          </motion.img>
+          <motion.img src={illustrator} id="illustrator" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>
+          <motion.img src={pr} id="pr" style={{width:"15%"}}
+            variants={skillsVariants}
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"> 
+          </motion.img>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
