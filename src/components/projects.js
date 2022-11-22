8d5e13058f57d5c8962c4864d3ea52ba8276f4f9
huifang <hui-f.gu@hotmail.com>
2022-11-22 14:43:55 +1030

    project

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,10 +7,11 @@ import photoguru from "./images/photoguru.png"
 import tictactoe from "./images/tictactoe.png"
 import "./projects.css"
 import { Container } from 'react-bootstrap';
+import { hover } from '@testing-library/user-event/dist/hover';
 function Projects() {
   return (
     <Container>
-      <Carousel>
+      <Carousel indicators={false}>
         <Carousel.Item>
           <Card className='w-100'>
             <Card.Img variant="top" src={jobtracker} />
@@ -23,10 +24,12 @@ function Projects() {
                 -use React Bootstrap for styling as well as React icons<br />
                 -use Adzuna api and Regex<br />
               </Card.Text>
-                <Button href='https://dry-bayou-44223.herokuapp.com/' className='me-1' variant="primary">Demo</Button>
-                <Button href='https://github.com/huifang719/jobtracker_server.git' variant="primary">Code</Button>
             </Card.Body>
           </Card>
+          <div className='w-6 d-flex justify-content-center mt-2'>
+            <Button href='https://dry-bayou-44223.herokuapp.com/' className='me-4' variant="primary">Demo</Button>
+            <Button href='https://github.com/huifang719/jobtracker_server.git' variant="primary">Code</Button>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
           <Card className='w-100'>
@@ -39,10 +42,12 @@ function Projects() {
                 -CRUD system using Express JS and PostgreSQL<br />
                 -use React Bootstrap, Bing map API , Fontawesome<br /><br />
               </Card.Text>
-                <Button href='https://find-your-baker.herokuapp.com/' className='me-1' variant="primary">Demo</Button>
-                <Button href='https://github.com/huifang719/find_your_baker.git' variant="primary">Code</Button>
             </Card.Body>
           </Card>
+          <div className='w-6 d-flex justify-content-center mt-2'>
+            <Button href='https://find-your-baker.herokuapp.com/' className='me-4' variant="primary">Demo</Button>
+            <Button href='https://github.com/huifang719/find_your_baker.git' variant="primary">Code</Button>
+          </div> 
         </Carousel.Item>
         <Carousel.Item>
           <Card className='w-100'>
@@ -55,10 +60,12 @@ function Projects() {
                   -implement CRUD system in Ruby and PostgreSQL<br />
                   -use bootstrap for styling<br /><br />
                 </Card.Text>
-                <Button href='https://arcane-oasis-62521.herokuapp.com/' className='me-1' variant="primary">Demo</Button>
-                <Button href='https://github.com/huifang719/PhotoGuru.git' variant="primary">Code</Button>
-              </Card.Body>
+              </Card.Body> 
             </Card> 
+            <div className='w-6 d-flex justify-content-center mt-2'>
+              <Button href='https://arcane-oasis-62521.herokuapp.com/' className='me-4' variant="primary">Demo</Button>
+              <Button href='https://github.com/huifang719/PhotoGuru.git' variant="primary">Code</Button>
+            </div>
         </Carousel.Item>
         <Carousel.Item>
           <Card className='w-100'>
@@ -71,10 +78,12 @@ function Projects() {
                   -using React Bootstrap for Styling and React typewriter<br />
                   -using Game of Throne Quote API <br />
                 </Card.Text>
-                  <Button href='https://tic-tac-toe-got.netlify.app/' className='me-1' variant="primary">Demo</Button>
-                  <Button href='https://github.com/huifang719/react_tictactoe.git' variant="primary">Code</Button>
               </Card.Body>
             </Card> 
+            <div className='w-6 d-flex justify-content-center mt-2'>
+              <Button href='https://tic-tac-toe-got.netlify.app/' className='me-4' variant="primary">Demo</Button>
+              <Button href='https://github.com/huifang719/react_tictactoe.git' variant="primary">Code</Button>
+            </div>
         </Carousel.Item>
       </Carousel>
       
