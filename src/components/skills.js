b429211e1de8f112f08040898efb1ec2e5300e56
huifang <hui-f.gu@hotmail.com>
2022-12-11 06:33:33 -0800

    add overlay on skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -18,6 +18,8 @@ import jest from './images/jest.png'
 import nodejs from './images/nodejs.png'
 import sinatra from './images/sinatra.png'
 import pr from './images/pr.png'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 function Skills() {
   const skillsVariants = {
@@ -53,114 +55,285 @@ function Skills() {
       <Row xs={1} md={3}>
         <Col style={{border:"1px solid lightgrey", height:"70vh",color:"lightgrey"}}>
           <h3 className="text-center">Languages</h3>
-          <motion.img src={javascript} id="js" style={{width:"20%"}} 
-            variants={skillsVariants}
-            initial='hidden'
-            animate='visible'
-            whileHover="hover">
-          </motion.img>  
-          <motion.img src={python} id="python" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover">  
-          </motion.img>
-          <motion.img src={ruby} id="ruby" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover='hover'>
-          </motion.img>
-          <motion.img src={html} id="html" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover='hover'>
-          </motion.img>
-          <motion.img src={css} id="css" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover='hover'>
-          </motion.img>
-          <motion.img src={psql} id="psql" style={{width:"20%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover='hover'>
-          </motion.img>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                JavaScript
+              </Tooltip>
+            }
+          >
+            <motion.img src={javascript} id="js" style={{width:"20%"}} 
+              variants={skillsVariants}
+              initial='hidden'
+              animate='visible'
+              whileHover="hover">
+            </motion.img> 
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Python
+              </Tooltip>
+            }
+          >
+            <motion.img src={python} id="python" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover">  
+            </motion.img>
+          </OverlayTrigger>
+          
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Ruby
+              </Tooltip>
+            }
+          >
+            <motion.img src={ruby} id="ruby" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover='hover'>
+            </motion.img>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                HTML
+              </Tooltip>
+            }
+          >
+            <motion.img src={html} id="html" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover='hover'>
+            </motion.img>
+          </OverlayTrigger>
+          
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                CSS
+              </Tooltip>
+            }
+          >
+            <motion.img src={css} id="css" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover='hover'>
+            </motion.img>
+          </OverlayTrigger>
+          
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                PostgreSQL
+              </Tooltip>
+            }
+          >
+            <motion.img src={psql} id="psql" style={{width:"20%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover='hover'>
+            </motion.img>
+          </OverlayTrigger>
+          
         </Col>
         <Col style={{border:"1px solid lightgrey", height:"70vh", color:"lightgrey"}}>
           <h3 className="text-center">Frameworks & Libraries</h3>
-          <motion.img src={react} id="react" style={{width:"20%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>
-          <motion.img src={bootstrap} id="bootstrap" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>     
-          <motion.img src={nodejs} id="nodejs" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>  
-          <motion.img src={express} id="express" style={{width:"20%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>  
-          <motion.img src={sinatra} id="sinatra" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>  
-          <motion.img src={jest} id="jest" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img> 
-          <motion.img src={fm} id="fm" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img> 
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                React JS
+              </Tooltip>
+            }
+          >
+            <motion.img src={react} id="react" style={{width:"20%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Bootstrap
+              </Tooltip>
+            }
+          >
+            <motion.img src={bootstrap} id="bootstrap" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img> 
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Node JS
+              </Tooltip>
+            }
+          >
+            <motion.img src={nodejs} id="nodejs" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Express
+              </Tooltip>
+            }
+          >
+            <motion.img src={express} id="express" style={{width:"20%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>  
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Sinatra
+              </Tooltip>
+            }
+          >
+            <motion.img src={sinatra} id="sinatra" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>  
+          </OverlayTrigger>
+          
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Jest
+              </Tooltip>
+            }
+          >
+            <motion.img src={jest} id="jest" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>  
+          </OverlayTrigger>
+     
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Framer Motion
+              </Tooltip>
+            }
+          >
+            <motion.img src={fm} id="fm" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>
+          </OverlayTrigger>
         </Col>
         <Col style={{border:"1px solid lightgrey", height:"70vh",color:"lightgrey"}}>
           <h3 className="text-center">Other Skills</h3>
-          <motion.img src={ps} id="ps" style={{width:"20%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>
-          <motion.img src={indesign} id="indesign" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover">          
-          </motion.img>
-          <motion.img src={illustrator} id="illustrator" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>
-          <motion.img src={pr} id="pr" style={{width:"15%"}}
-            variants={skillsVariants}
-            initial="hidden"
-            animate="visible"
-            whileHover="hover"> 
-          </motion.img>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                InDesign
+              </Tooltip>
+            }
+          >
+            <motion.img src={indesign} id="indesign" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover">          
+            </motion.img>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Illustrator
+              </Tooltip>
+            }
+          >
+            <motion.img src={illustrator} id="illustrator" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                PhotoShop
+              </Tooltip>
+            }
+          >
+            <motion.img src={ps} id="ps" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>
+          </OverlayTrigger>
+          
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip>
+                Premiere
+              </Tooltip>
+            }
+          >
+            <motion.img src={pr} id="pr" style={{width:"15%"}}
+              variants={skillsVariants}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"> 
+            </motion.img>
+          </OverlayTrigger>
+          
+          
+          
         </Col>
       </Row>
     </Container>
